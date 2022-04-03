196c79369322746307b457f54d52dfc402934614
OjusWiZard <ojuswimail@gmail.com>
2022-04-04 02:03:38 +0530

    heroku staticfiles support
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

--- Zealicon/settings.py
+++ Zealicon/settings.py
@@ -89,6 +89,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "Zealicon.urls"
@@ -162,8 +163,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 
 # If Cloudinary credentials are present in the environment
